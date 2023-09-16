f92c011ece5090c2e3f4b56ea75a85429d86c88a
ifeconastar <ifeanyiizuegbu29@gmail.com>
2023-09-16 02:36:49 -0700

    async fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+"""A coroutine that takes no arguments"""
+
+
 import asyncio
-import random
+from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    '''Generates a sequence of 10 numbers.
-    '''
-    for _ in range(10):
+    """loops 10x and yield a random number b/w 0 & 10"""
+    for i in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+
+        yield uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-'''Task 1's module.
-'''
+"""A coroutine that collects 10 random
+numbers from another coroutine"""
+
+
+import asyncio
 from typing import List
-from importlib import import_module as using
 
-
-async_generator = using('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number generator.
-    '''
-    return [num async for num in async_generator()]
+    """ Returns a random number retrieved from a coroutine"""
+    result = [i async for i in async_generator()]
+    return result
